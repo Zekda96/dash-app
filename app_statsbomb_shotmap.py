1abab6f0f80144d24acc61a4bd1eb3a89cf27eab
dgranjac <dgranja@opus2.com>
2023-08-29 17:34:36 -0500

    Created shotmap app

---
+++ app_statsbomb_shotmap.py
@@ -0,0 +1,111 @@
+import plotly.express as px
+import plotly.graph_objects as go
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import pandas as pd
+from statsbombpy import sb
+#
+from pitchly import Pitch
+import statsbomb_invert as sbi
+
+# Read Data
+
+events = pd.read_csv('match_69302.csv')
+
+
+new_df = events[['team', 'player', 'type', 'timestamp', 'period', 'location', 'shot_statsbomb_xg',
+                 'shot_body_part', 'shot_technique', 'shot_outcome', 'shot_end_location']]
+
+shots_df = new_df[(new_df['type'] == 'Shot')]
+
+# Separate coordinates
+lis = shots_df.location.str.split(',', expand=True)
+shots_df['x'] = pd.to_numeric(lis[0].str.strip('['))
+shots_df['y'] = pd.to_numeric(lis[1].str.strip(']'))
+
+lis = shots_df.shot_end_location.str.split(',', expand=True)
+shots_df['end_x'] = pd.to_numeric(lis[0].str.strip('['))
+shots_df['end_y'] = pd.to_numeric(lis[1].str.strip(']'))
+shots_df['end_z'] = pd.to_numeric(lis[2].str.strip(']'))
+
+# shots_df[['x', 'y']] = shots_df['location'].apply(pd.Series)
+# shots_df[['end_x', 'end_y', 'end_z']] = shots_df['shot_end_location'].apply(pd.Series)
+
+# Convert from statsbomb yards to meters
+shots_df['x'] = sbi.convert_x(shots_df['x'])
+shots_df['y'] = sbi.convert_y(shots_df['y'])
+
+shots_df['end_x'] = sbi.convert_x(shots_df['end_x'])
+shots_df['end_y'] = sbi.convert_y(shots_df['end_y'])
+
+
+# --------------------------------DASH APP--------------------------------------
+
+# Sample data for demonstration
+df = shots_df
+
+# Create the Dash app
+app = dash.Dash(__name__)
+
+# Layout of the app
+app.layout = html.Div([
+    dcc.Checklist(
+        id='scatter-checklist',
+        options=[
+            {'label': 'Goal', 'value': 'Goal'},
+            {'label': 'Post', 'value': 'Post'},
+            {'label': 'Off Target', 'value': 'Off T'}
+        ],
+        value=['Goal'],
+        inline=True
+    ),
+    dcc.Graph(id='scatter-plot')
+])
+
+
+# Callback to update the scatter plot based on checklist selection
+@app.callback(
+    Output('scatter-plot', 'figure'),
+    [Input('scatter-checklist', 'value')]
+)
+def update_scatter(vals):
+    pitch = Pitch()
+    fig = pitch.plot_pitch(show=False)
+    fig.update_layout(title_text='Pass map')
+    for val in vals:
+        dff = df[df['shot_outcome'] == val]
+        fig = fig.add_scatter(x=dff['x'], y=dff['y'],
+                              name=f'{val}',
+                              mode="markers",
+                              marker=dict(
+                                  size=14,
+                                  color='rgba(135, 206, 250, 1)',
+                                  line_width=3,
+                                  line_color='black'
+                              )
+                              )
+
+    # Style Goals
+    fig.update_traces(selector=dict(type="scatter", name='Goal'),
+                      marker=dict(color='red',
+                                  ),
+                      )
+
+    # Style Off Target
+    fig.update_traces(selector=dict(type="scatter", name='Off T'),
+                      marker=dict(color='red',
+                                  symbol='x-thin'
+                                  ),
+                      )
+
+    return fig
+
+    # Set y-axis range
+    # fig.update_yaxes()
+
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
---
+++ pitchly.py
@@ -0,0 +1,258 @@
+from collections import namedtuple
+import plotly.graph_objects as go
+from params import prm
+
+
+class Pitch:
+    def __init__(self):
+        # ALL DIMENSIONS IN m
+        self.border_dimen = (
+            3,
+            3,
+        )  # include a border arround of the field of width 3m
+        self.meters_per_yard = 0.9144  # unit conversion from yards to meters
+        self.half_pitch_length = prm.field_dim[0] / 2.0  # length of half pitch
+        self.half_pitch_width = prm.field_dim[1] / 2.0  # width of half pitch
+        self.signs = [-1, 1]
+        # Soccer field dimensions typically defined in yards, so we need to
+        # convert to meters
+        self.goal_line_width = 8 * self.meters_per_yard
+        self.box_width = 20 * self.meters_per_yard
+        self.box_length = 6 * self.meters_per_yard
+        self.area_width = 44 * self.meters_per_yard
+        self.area_length = 18 * self.meters_per_yard
+        self.penalty_spot = 12 * self.meters_per_yard
+        self.corner_radius = 1 * self.meters_per_yard
+        self.D_length = 8 * self.meters_per_yard
+        self.D_radius = 10 * self.meters_per_yard
+        self.D_pos = 12 * self.meters_per_yard
+        self.centre_circle_radius = 10 * self.meters_per_yard
+
+        self.point = namedtuple("point", ["x", "y"])
+        self.centre = self.point(0.0, 0.0)
+
+    def get_layout(
+        self,
+        time=None,
+        frameID=None,
+        frame_range=None,
+        title=None,
+        pitch_control=False,
+    ):
+        if pitch_control:
+            field_markings_color = prm.pc["field_markings_color"]
+            field_color = prm.pc["field_color"]
+            title_color = prm.pc["title_color"]
+        else:
+            field_markings_color = prm.std["field_markings_color"]
+            field_color = prm.std["field_color"]
+            title_color = prm.std["title_color"]
+
+        shapes = []
+
+        mid_circle = dict(
+            type="circle",
+            x0=self.centre.x - self.centre_circle_radius,
+            y0=self.centre.y - self.centre_circle_radius,
+            x1=self.centre.x + self.centre_circle_radius,
+            y1=self.centre.y + self.centre_circle_radius,
+            line_color=field_markings_color,
+            layer="below",
+        )
+        mid_line = dict(
+            type="line",
+            x0=self.centre.x,
+            y0=self.centre.y - self.half_pitch_width,
+            x1=self.centre.x,
+            y1=self.centre.y + self.half_pitch_width,
+            line_color=field_markings_color,
+            layer="below",
+        )
+        mid_point = dict(
+            type="circle",
+            x0=self.centre.x - 0.4,
+            y0=self.centre.y - 0.4,
+            x1=self.centre.x + 0.4,
+            y1=self.centre.y + 0.4,
+            line_color=field_markings_color,
+            fillcolor=field_markings_color,
+            layer="below",
+        )
+        shapes.extend([mid_circle, mid_line, mid_point])
+
+        for s in self.signs:
+
+            # plot pitch boundary
+            boundary1 = dict(
+                type="line",
+                x0=-self.half_pitch_length,
+                y0=s * self.half_pitch_width,
+                x1=self.half_pitch_length,
+                y1=s * self.half_pitch_width,
+                line_color=field_markings_color,
+                layer="below",
+            )
+
+            boundary2 = dict(
+                type="line",
+                x0=s * self.half_pitch_length,
+                y0=-self.half_pitch_width,
+                x1=s * self.half_pitch_length,
+                y1=self.half_pitch_width,
+                line_color=field_markings_color,
+                layer="below",
+            )
+
+            circle = dict(
+                type="circle",
+                x0=s
+                * (self.centre.x + self.half_pitch_length - self.penalty_spot)
+                - self.centre_circle_radius,
+                y0=self.centre.y - self.centre_circle_radius,
+                x1=s
+                * (self.centre.x + self.half_pitch_length - self.penalty_spot)
+                + self.centre_circle_radius,
+                y1=self.centre.y + self.centre_circle_radius,
+                line_color=field_markings_color,
+                layer="below",
+            )
+
+            patch = dict(
+                type="rect",
+                x0=s * self.half_pitch_length,
+                y0=-self.area_width / 2.0 - 1,
+                x1=s * (self.half_pitch_length - self.area_length),
+                y1=self.area_width / 2.0 + 1,
+                line=dict(color=field_color, width=0),
+                fillcolor=field_color,
+                layer="below",
+            )
+
+            box = dict(
+                type="rect",
+                x0=s * self.half_pitch_length,
+                y0=-self.area_width / 2.0,
+                x1=s * (self.half_pitch_length - self.area_length),
+                y1=self.area_width / 2.0,
+                line=dict(color=field_markings_color, width=2),
+                layer="below",
+            )
+            D = dict(
+                type="rect",
+                x0=s * self.half_pitch_length,
+                y0=-self.box_width / 2.0,
+                x1=s * (self.half_pitch_length - self.box_length),
+                y1=self.box_width / 2.0,
+                line=dict(color=field_markings_color, width=2),
+                layer="below",
+            )
+            pen = dict(
+                type="circle",
+                x0=s * (self.half_pitch_length - self.penalty_spot) - 0.4,
+                y0=-0.4,
+                x1=s * (self.half_pitch_length - self.penalty_spot) + 0.4,
+                y1=0.4,
+                line_color=field_markings_color,
+                fillcolor=field_markings_color,
+                layer="below",
+            )
+
+            top_post = dict(
+                type="rect",
+                x0=s * self.half_pitch_length,
+                y0=self.goal_line_width / 2.0 - 0.5,
+                x1=s * (self.half_pitch_length - 0.5),
+                y1=self.goal_line_width / 2.0 + 0.5,
+                line=dict(color=field_markings_color, width=0),
+                fillcolor=field_markings_color,
+                layer="below",
+            )
+
+            bottom_post = dict(
+                type="rect",
+                x0=s * self.half_pitch_length,
+                y0=-self.goal_line_width / 2.0 - 0.5,
+                x1=s * (self.half_pitch_length - 0.5),
+                y1=-self.goal_line_width / 2.0 + 0.5,
+                line=dict(color=field_markings_color, width=0),
+                fillcolor=field_markings_color,
+                layer="below",
+            )
+
+            shapes.extend(
+                [
+                    boundary1,
+                    boundary2,
+                    circle,
+                    patch,
+                    box,
+                    D,
+                    pen,
+                    top_post,
+                    bottom_post,
+                ]
+            )
+
+            # set axis limits
+        xmax = prm.field_dim[0] / 2.0 + self.border_dimen[0]
+        ymax = prm.field_dim[1] / 2.0 + self.border_dimen[1]
+
+        layout = go.Layout(
+            title={
+                "text": title,
+                "y": 0.98,
+                "x": 0.5,
+                "xanchor": "center",
+                "yanchor": "top",
+                "font_color": title_color,
+                "font_size": 20,
+            },
+            hovermode="closest",
+            autosize=True,
+            width=prm.field_width,
+            height=prm.field_height,
+            plot_bgcolor=field_color,
+            xaxis=go.layout.XAxis(
+                range=[-xmax, xmax],
+                showgrid=False,
+                zeroline=False,
+                showticklabels=False,
+                visible=False,
+            ),
+            yaxis=go.layout.YAxis(
+                range=[-ymax, ymax],
+                showgrid=False,
+                zeroline=False,
+                showticklabels=False,
+                visible=False,
+                scaleanchor="x",
+                scaleratio=1,
+            ),
+            margin=go.layout.Margin(l=0, r=0, b=0, t=0, pad=0),
+            legend=dict(
+                x=0.1, y=0.993, orientation="h", bgcolor="rgba(0,0,0,0)"
+            ),
+        )
+
+        layout["shapes"] = shapes
+
+        # if title:
+        #     layout["title_text"] = title
+
+        return layout
+
+
+    def plot_pitch(self, show=True):
+        """Just generates an empty pitch.
+        Store it in a fig object and add data to plot over it.
+        """
+        fig_dict = {"data": [], "layout": {}, "frames": []}
+
+        fig_dict["layout"] = self.get_layout()
+
+        fig = go.Figure(fig_dict)
+
+        if show:
+            fig.show()
+
+        return fig
