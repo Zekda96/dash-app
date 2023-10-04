03e8eff07344235c229a5cd5ae1b459f522ab99f
Zekda96 <danielgranjac@gmail.com>
2023-10-04 18:22:02 -0500

    Gets data from european competitions as well

--- scrapers/scrape_fbref_nationality.py
+++ scrapers/scrape_fbref_nationality.py
@@ -2,12 +2,12 @@
 import numpy as np
 import pandas as pd
 from datetime import date
-from bs4 import BeautifulSoup
-import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 import flag
+
+country = 'ECUADOR'
 
 # fbref table link
 url = 'https://fbref.com/es/stathead/scout/ECU'
@@ -66,7 +66,12 @@ if today.month < 10:
 else:
     mes = f'{today.month}'
 
-hoy = f'{today.year}-{mes}-{today.day}' # Today's date in dataframe's format
+if today.day < 10:
+    dia = f'0{today.day}'
+else:
+    dia = f'{today.day}'
+
+hoy = f'{today.year}-{mes}-{dia}' # Today's date in dataframe's format
 
 
 fixtures_today = fixtures[fixtures['Fecha'] == hoy]
@@ -99,3 +104,114 @@ print(tweet)
 
 with open('fixtures.txt', 'w') as f:
     f.write(tweet)
+
+# ------------------------- ADD CUP SCHEDULES
+url_c1 = 'https://fbref.com/es/comps/8/nacionalidades/Nacionalidades-en-Champions-League'
+url_c2 = 'https://fbref.com/es/comps/19/nacionalidades/Nacionalidades-en-Europa-League'
+url_c3 = 'https://fbref.com/es/comps/882/nacionalidades/Nacionalidades-en-Europa-Conference-League'
+competitions = [{'name': 'Champions Lg', 'url': url_c1},
+                {'name': 'Europa Lg',    'url': url_c2},
+                {'name': 'Conf Lg', 'url': url_c3}
+                ]
+
+
+cups = []
+for cup in competitions:
+    driver.get(cup['url'])
+    # Get column names
+    thead = driver.find_element(By.XPATH, '//*[@id="nations"]/thead')
+    header = thead.find_element(By.XPATH, './/tr')
+    # Split header
+    cols = header.text.split()
+    # Fix column name as it is split into separate columns
+    idx = cols.index('N.°')
+    cols[idx] = 'N.° de Jugadores'
+    del cols[idx+1:idx+3]  # Delete the previous separate columns
+
+    # Get rows from cup nationalities table
+    tbody = driver.find_element(By.XPATH, '//*[@id="nations"]/tbody')
+
+    # Iterate rows and find Ecuador row
+    for tr in tbody.find_elements(By.XPATH, './/tr'):
+        if tr.text.split()[2] == country.capitalize():
+            country_players = []
+            country_url = []
+            print(f'\nHoy juegan por {cup["name"]}:')
+            # Iterate through ecuadorean players and get their profile url
+            for player in tr.find_elements(By.XPATH, './/td[4]/a'):
+                name = player.text
+                player_url = player.get_attribute('href')
+
+                print(f'{name}')
+                country_players.append(name)
+                country_url.append(player_url)
+
+    # Iterate through players profile url to get their teams profile url
+    teams_url = []
+    team_name = []
+    for url in country_url:
+        driver.get(url)
+        for p in driver.find_elements(By.XPATH, '//*[@id="meta"]/div/p'):
+            for strong in p.find_elements(By.XPATH, './/strong'):
+
+                # Get club name and club profile URL
+                if strong.text == 'Club :':
+                    a = p.find_element(By.XPATH, './/a')
+                    team_url = a.get_attribute('href')
+                    team_name.append(a.text)
+                    teams_url.append(team_url)
+                    print(f'{cup["name"]} urls: {team_url}')
+                    # teams_url.append(p.find_element(By.XPATH, './/a').get_attribute('href'))
+
+    cup_data = []
+    team_data = []
+    # Iterate through teams profiles to check if they play cup today
+    for i, url in enumerate(teams_url):
+        driver.get(url)
+        # Iter rows and check date and comp
+        for tr in driver.find_elements(By.XPATH, '//*[@id="matchlogs_for"]/tbody/tr[not(contains(@class, "thead rowSum"))]'):
+            # If is not a header row
+            if tr.text.split()[0] != 'Fecha':
+                # Check if team plays today
+                is_today = tr.find_element(By.XPATH, './/th').text == hoy
+                is_cup = tr.find_element(By.XPATH, './/td[2]').text == cup['name']
+                if is_today and is_cup:
+                    print(f'{cup["name"]}: YES')
+                    data = []
+                    cols = []
+                    for d in tr.find_elements(By.XPATH, './/td'):
+                        data.append(d.text)
+
+                    header = driver.find_element(By.XPATH, '//*[@id="matchlogs_for"]/thead/tr')
+                    cols = header.text.split()[1:]
+                    # Fix column name as it is split into separate columns
+                    idx = cols.index('Informe')
+                    cols[idx] = 'Informe del Partido'
+                    del cols[idx+1:idx+3]  # Delete the previous separate columns
+
+                    break
+
+        # Append match info for qualifying matches
+        if is_today and is_cup:
+            team_data.append(pd.Series(data))
+        else:
+            print(f'{cup["name"]}: NO')
+    #
+    if len(team_data) > 0:
+        cup_data = pd.concat(team_data, axis=1, ignore_index=True).T
+        cup_data.columns = cols
+        cup_data.insert(0, 'Jugador', country_players)
+        cup_data.insert(1, 'Equipo', team_name)
+        cups.append(cup_data)
+
+
+for cup in cups:
+    tweet = [f'\n{cup.Comp[0]}:\n']
+    for row in cup.itertuples():
+        j = row.Jugador
+        t = row.Equipo
+        tweet.append(f'{j} - {t}\n')
+
+    tweet = ''.join(tweet)
+    print(tweet)
+
